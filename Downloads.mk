820baf37f19ed04ab54415b389f868b8a2aac0d6
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-12-15 20:03:27 -0500

    Leaving attic

--- Downloads.mk
+++ Downloads.mk
@@ -1,8 +1,26 @@
 runscreen: ;
 
+vim_session: 
+	bash -cl "vmt"
+
+######################################################################
+
 folders += stash library cloud reviewDocs sent
 
 folders: $(folders)
 
 $(folders):
 	/bin/ln -s ~/screens/org/Planning/$@ .
+
+clean:
+	$(RM) $(folders)
+
+######################################################################
+
+update_copies: .
+	rename -f "s/ *\([0-9]\)//" *\([0-9]\).*
+	touch $@
+
+quote_names:
+	rename -f "s/'//" *.*
+
