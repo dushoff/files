cf87983049f661d36ebfb5f3b14d08450841181e
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-11-11 21:51:31 -0500

    attic

--- Downloads.mk
+++ Downloads.mk
@@ -1,7 +1,10 @@
-
-default: update_copies
+## This is the ~/Downloads Makefile; it is a file in files/ and you are supposed to link it into Downloads
 
 ## There are things here that are intended as mirrors but not maintained since nextcloud collapse. Maybe make them as links to where. files?
+
+## Maybe maintained now. I seem some mirrors, but not a lot of explanation
+
+default: update_copies
 
 vim_session: 
 	bash -cl "vm ~/screens/org/files/Downloads.md"
