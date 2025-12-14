f3355b31e8df1f67d4a07206a7792173d2c7f730
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-12-14 18:54:44 +0100

    Sevilla!

--- Downloads.mk
+++ Downloads.mk
@@ -1,2 +1,66 @@
 ## Avoid doing stuff here; that's what files is for
+## This is the ~/Downloads make file; managed in org/files
+## Actual Makefile should just include this file – or link, I guess
+all: update_copies
+runscreen: ;
+
+default: update_copies
+
+## There are things here that are intended as mirrors but not maintained since nextcloud collapse. Maybe make them as links to where. files?
+
+## Maybe maintained now. I seem some mirrors, but not a lot of explanation
+
+default: update_copies
+
+vim_session: 
+	bash -cl "vm ~/screens/org/files/Downloads.md"
+
+mirrors += jd picture transit attach
+
+######################################################################
+## Moving stuff around
+
+new:
+	mkdir -p $@
+	cp *.zip $@
+	cd $@ && bash -cl lastunzip && $(RM) *.zip
+
+## What is this for?? Should I delete and see if I re-invent it? 2025 Sep 27 (Sat)
+%.contents: contents
+	mv $< $*
+
+contents: 
+	mkdir $@
+	mv *.* $@
+
+######################################################################
+
+update_copies: .
+	rename -f "s/ *\([0-9]\)//" *\([0-9]\).*
+	touch $@
+
+quote_names:
+	rename -f "s/'//" *.*
+
+filenames: ..filenames ;
+%.filenames:
+	rename "s/[()& ,?!-]+/_/g" $*/*.*
+
+## Resting
+
+folders += stash library cloud reviewDocs sent
+
+folders: $(folders)
+
+$(folders):
+	/bin/ln -s ~/screens/org/Planning/$@ .
+
+nuke: delall clean up
+
+delall:
+	$(RM) *.*
+
+clean:
+	$(RM) $(folders)
+
 include ~/screens/org/files/Downloads.mk
